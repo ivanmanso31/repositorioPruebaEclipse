542beb4c35f23f881cc93daa7c42aac6495b6e51
IvanManso <ivan.mg313@gmail.com>
2022-02-01 14:31:17 +0100

    muchos cambios

--- Nuevo/src/HolaMundo.java
+++ Nuevo/src/HolaMundo.java
@@ -1,5 +1,22 @@
 
 public class HolaMundo {
 	
-	int aux;
+	String saludo;
+	
+	public HolaMundo() {
+		
+	}
+
+	public String getSaludo() {
+		return saludo;
+	}
+
+	public void setSaludo(String saludo) {
+		this.saludo = saludo;
+	}
+
+	public void saludo() {
+		saludo="Hola mundo";
+		System.out.println(saludo);
+	}
 }
---
+++ Nuevo/src/PruebaHolaMundo.java
@@ -0,0 +1,12 @@
+
+public class PruebaHolaMundo {
+
+	public static void main(String[] args) {
+		
+		HolaMundo h1=new HolaMundo();
+		h1.saludo();
+			
+		}
+
+	}
+
