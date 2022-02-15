58a64fa4659cda5e111071c1f5d7f1c4bb5fc52a
IvanManso <ivan.mg313@gmail.com>
2022-02-15 14:09:12 +0100

    Clase Multiplicacion

---
+++ Nuevo/src/Multiplicacion.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Multiplicacion {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Primer numero");
+		int op1=sc.nextInt();
+		System.out.println("Segundo numero");
+		int op2=sc.nextInt();
+		System.out.println("El resultado es: "+(op1*op2));
+	}
+
+}
